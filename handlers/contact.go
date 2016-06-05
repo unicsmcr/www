e0e1396c93f4f07447424bc25da810d4b86a8a40
andrei.muntean.dev@gmail.com <andrei.muntean.dev@gmail.com>
2016-06-05 22:52:54 +0100

    Created the contact page

--- handlers/contact.go
+++ handlers/contact.go
@@ -1,7 +1,21 @@
 package handlers
 
-import "net/http"
+import (
+	"os"
+	"net/http"
+)
+
+var reCaptchaSiteKey = os.Getenv("RECAPTCHA_SITE_KEY")
 
 func contact(w http.ResponseWriter, r *http.Request) {
-	templates["comingsoon"].ExecuteTemplate(w, "layout", nil)
+	switch r.Method {
+		case "GET":
+			templates["contact"].ExecuteTemplate(w, "layout", reCaptchaSiteKey)
+			
+		case "POST":
+			templates["contact"].ExecuteTemplate(w, "layout", reCaptchaSiteKey)			
+			
+		default:
+			errorHandler(w, r, http.StatusBadRequest)
+	}
 }
